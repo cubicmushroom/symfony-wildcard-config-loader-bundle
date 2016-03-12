4d6f0f98d1eeb26a1ffe46fa03be35bd0430d910
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-03-12 16:26:48 +0000

    Added support for glob pattern file locating

--- spec/GlobFileLocatorSpec.php
+++ spec/GlobFileLocatorSpec.php
@@ -19,6 +19,22 @@ class GlobFileLocatorSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType(GlobFileLocator::class);
+        /** @noinspection PhpUndefinedMethodInspection */
         $this->shouldBeAnInstanceOf(FileLocator::class);
+    }
+
+
+    function it_should_locate_file_with_a_glob_pattern()
+    {
+        /** @var self|GlobFileLocator $this */
+
+        $testClientFilePath = realpath(__DIR__ . '/../test_files/clients');
+
+        $this->locate('clients/*', __DIR__ . '/../test_files', false)->shouldReturn(
+            [
+                $testClientFilePath . '/123.yml',
+                $testClientFilePath . '/456.yml',
+            ]
+        );
     }
 }
